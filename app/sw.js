a907785388b7eac01acafeb5bd8649d5e497ca74
Carlos Sechi <carlos.sechi@zrp.com.br>
2020-12-13 02:11:05 -0300

    Update dependencies

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.3/workbox-sw.js');
 
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
